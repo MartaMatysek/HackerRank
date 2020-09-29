e1dbbb08914c7c3181d74c691f193364212c8c82
Marta <m.matysek95@wp.pl>
2020-09-29 23:44:48 +0200

    Sales by Match (warm-up) solution

---
+++ interview-preparation-kit/src/warm_up/SocksMatcher.java
@@ -0,0 +1,32 @@
+package warm_up;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SocksMatcher {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        var howMany = in.nextInt();
+        int colors[] = new int[howMany];
+        for(int i = 0; i < howMany; i++){
+            colors[i] = in.nextInt();
+        }
+
+        System.out.println(sockMerchant(howMany, colors));
+    }
+
+    static int sockMerchant(int n, int[] ar) {
+        Arrays.sort(ar);
+        int pairs = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (ar[i] == ar[i+1]) {
+                pairs++;
+                i++;
+            }
+        }
+
+        return pairs;
+    }
+}
